1d9abaed28638bf2c7befcc1aa351cf93f35e670
Christian Foote <christianfoote999@gmail.com>
2022-04-30 23:06:31 -0400

    added inquier prompts

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager')
+
+
+const promptMenu = () =>{
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            choices: ['intern','engineer','save and exit'],
+          }]).then(answer =>{
+                switch (answer.choice){
+                    case 'intern':
+                        var intern = new intern();
+                        break;
+                    case 'engineer': 
+                        var engineer = new engineer();
+                        break;
+                }
+          });
+}
+
+//var manager = new Manager();
+promptMenu();
